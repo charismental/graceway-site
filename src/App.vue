630db0b0e842a06e054edc39a725605d39c6e138
Tristin <thewett-c@sbgtv.com>
2020-11-24 07:26:14 -0800

    Slideshow to Home view / populate sideNav with router links / prominent donate link / fix issue with hamburger menu offscreen

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,54 @@
 <template>
   <v-app>
-        <v-navigation-drawer
-      v-model="openSideNav"
-      right
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-title>Foo</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Bar</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Fizz</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Buzz</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar elevate-on-scroll app color="white">
       <div class="d-flex align-center">
         <v-img
           alt="Graceway Radio Logo"
           contain
-          class="ml-4 mt-2"
+          class="mt-2"
+          :class="isMobile ? '' : 'ml-4'"
           src="@/assets/logo.png"
           max-height="60"
           max-width="60"
         />
       </div>
-      <v-toolbar-title class="text-h5 ml-2"> Graceway Radio </v-toolbar-title>
+      <v-toolbar-title class="ml-2">
+        <div :class="$vuetify.breakpoint.smAndUp ? 'text-h5' : 'text-body-1'">
+          Graceway Radio
+        </div>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        text
-        active-class="active-button"
-        exact
-        class="mr-1"
-        small
-        :to="{ name: 'Home' }"
-        >Home</v-btn
-      >
-      <v-btn text class="mr-1" small :to="{ name: 'About' }"
-        >Program Schedule</v-btn
-      >
-      <v-btn text class="mr-1" small>How to Listen</v-btn>
-      <v-btn text class="mr-1" small>Blog</v-btn>
-      <v-btn text class="mr-1" small>Contact</v-btn>
-      <v-btn text class="mr-1" small>Make a Donation</v-btn>
-      <v-app-bar-nav-icon @click.stop="openSideNav = !openSideNav"></v-app-bar-nav-icon>
+      <div v-if="!isMobile">
+        <v-btn
+          v-for="navItem in navItems"
+          :key="navItem.name"
+          :text="!navItem.featured"
+          active-class="active-button"
+          class="mr-2"
+          :color="navItem.featured ? 'warning' : ''"
+          exact
+          small
+          :to="navItem.link"
+          >{{ navItem.name }}</v-btn
+        >
+      </div>
+      <v-app-bar-nav-icon
+        v-if="isMobile"
+        @click.stop="openSideNav = !openSideNav"
+      ></v-app-bar-nav-icon>
     </v-app-bar>
+    <v-navigation-drawer v-model="openSideNav" right app temporary>
+      <v-list nav dense>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item v-for="navItem in navItems" :key="navItem.name">
+            <v-list-item-title @click="navTo(navItem.link)">{{ navItem.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <v-container fluid class="px-6">
-        <!-- style="transform:rotate(-90deg)" -->
-        <v-carousel v-model="model" height="320" :show-arrows="false" cycle>
-          <v-carousel-item v-for="(color, i) in colors" :key="color">
-            <v-sheet :color="color" height="100%" tile>
-              <v-row class="fill-height" align="center" justify="center">
-                <div class="display-3">Slide {{ i + 1 }}</div>
-              </v-row>
-            </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
-      </v-container>
+      <router-view />
     </v-main>
   </v-app>
 </template>
@@ -88,10 +60,50 @@ export default {
   components: {},
 
   data: () => ({
-    model: 0,
     openSideNav: false,
-    colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
+    navItems: [
+      {
+        name: 'Home',
+        link: { name: 'Home' },
+        featured: false,
+      },
+      {
+        name: 'Program Schedule',
+        link: { name: 'ProgramSchedule' },
+        featured: false,
+      },
+      {
+        name: 'How To Listen',
+        link: { name: 'HowToListen' },
+        featured: false,
+      },
+      {
+        name: 'Blog',
+        link: { name: 'Blog' },
+        featured: false,
+      },
+      {
+        name: 'Contact',
+        link: { name: 'Contact' },
+        featured: false,
+      },
+      {
+        name: 'Make A Donation',
+        link: { name: 'Donate' },
+        featured: true,
+      },
+    ],
   }),
+  methods: {
+    navTo(linkObj) {
+      this.$router.push(linkObj);
+    },
+  },
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    },
+  },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="home">
-  </div>
+  <v-container fluid class="px-6">
+    <!-- style="transform:rotate(-90deg)" -->
+    <v-carousel v-model="model" height="320" :show-arrows="false" cycle>
+      <v-carousel-item v-for="(color, i) in colors" :key="color">
+        <v-sheet :color="color" height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="display-3">Slide {{ i + 1 }}</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </v-container>
 </template>
 
 <script>
-
 export default {
   name: 'Home',
   components: {},
+  data: () => ({
+    model: 0,
+    colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
+  }),
 };
 </script>
