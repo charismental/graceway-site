9a5d06f132bd7059b0d4abdb893d07bd39f5c378
lordchuffnel <fs-j@live.com>
2020-11-25 16:09:41 -0800

    initial array for speakers
    added array in data.
    added thumbnails to assets/speakers

--- src/views/ProgramSchedule.vue
+++ src/views/ProgramSchedule.vue
@@ -7,6 +7,60 @@
 
 export default {
   name: 'ProgramSchedule',
+  data: () => ({
+    speakers: [
+      {
+        name: 'Alistair Begg',
+        photo: '',
+        times: 'Weekdays @ 1:00AM/PM'
+      },
+      {
+        name: 'David Pawson',
+        photo: '',
+        times: 'Mon, Wed, Fri @ 2:30AM/PM'
+      },
+      {
+        name: 'John Casteel',
+        photo: '',
+        times: 'Mon, Wed, Fri - 6:30AM, 7:00PM'
+      },
+      {
+        name: 'Paul Washer',
+        photo: '',
+        times: 'Mon, Wed, Fri - 8:30AM, 9:00PM'
+      },
+      {
+        name: 'Chuck Missler',
+        photo: '',
+        times: 'Weekdays - 10:30AM, 11:00PM'
+      },
+      {
+        name: 'Curtis Dougherty',
+        photo: '',
+        times: 'Mon, Wed, Fri @ 4:30AM/PM'
+      },
+      {
+        name: 'Ern Baxter',
+        photo: '',
+        times: 'Tues, Thur @ 2:30AM/PM'
+      },
+      {
+        name: 'Jamie Buckingham',
+        photo: '',
+        times: 'Tues, Thur @ 4:30AM/PM'
+      },
+      {
+        name: 'David Wilkerson',
+        photo: '',
+        times: 'Tues, Thur - 8:30AM, 9:00PM'
+      },
+      {
+        name: 'Derek Prince',
+        photo: '',
+        times: ''
+      }
+    ],
+  }),
   components: {},
 };
 </script>
