2d64a1799891b44fd5b601489a759f44b519d009
Tristin Hewett <thewett-c@sbgtv.com>
2020-12-12 22:27:21 -0800

    Attempting fix for shrinking fixed content take #2

--- src/App.vue
+++ src/App.vue
@@ -288,6 +288,17 @@ export default {
 };
 </script>
 
+<style>
+body {
+  min-height: 100vh;
+  min-height: -webkit-fill-available;
+}
+
+html {
+  height: -webkit-fill-available;
+}
+</style>
+
 <style lang="scss" scoped>
 .active-button {
   color: blue;
