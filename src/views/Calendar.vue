54a1d4e626a7646728716810708aa5f6295cab36
frank <paintersmechanic@gmail.com>
2021-02-11 10:03:25 -0800

    added scroll to time
    added scroll to time functionality

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -10,7 +10,15 @@
           :start="dailyStartDay"
           end="2021-02-05"
           :event-color='getEventColor'
-        ></v-calendar>
+        >
+        <template v-slot:day-body="{ date, week }">
+          <div
+          class="v-current-time"
+          :class="{ first: date === week[0].date }"
+          :style="{ top: nowY }"
+          ></div>
+        </template>
+        </v-calendar>
       </v-sheet>
     </v-col>
   </v-row>
@@ -20,6 +28,8 @@
 export default {
   data: () => ({
     weekday: [1, 2, 3, 4, 5],
+    value: '',
+    ready: false,
     events: [
       {
         name: 'Alistair Begg',
@@ -384,6 +394,13 @@ export default {
     ],
   }),
   computed: {
+    cal() {
+      return this.ready ? this.$refs.calendar : null;
+    },
+    nowY() {
+      // eslint-disable-next-line prefer-template
+      return this.cal ? this.cal.timeToY(this.cal.times.now) + 'px' : '-10px';
+    },
     calendarType() {
       if (this.$vuetify.breakpoint.smAndDown) {
         return 'day';
@@ -403,11 +420,47 @@ export default {
     getEventColor(event) {
       return event.color;
     },
+    getCurrentTime() {
+      return this.cal ? this.cal.times.now.hour * 60 + this.cal.times.now.minute : 0;
+    },
+    scrollToTime() {
+      const time = this.getCurrentTime();
+      const first = Math.max(0, time - (time % 30) - 30);
+
+      this.cal.scrollToTime(first);
+    },
+    updateTime() {
+      setInterval(() => this.cal.updateTimes(), 60 * 1000);
+    },
+  },
+  mounted() {
+    this.ready = true;
+    this.scrollToTime();
+    this.updateTime();
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.v-current-time {
+  height: 2px;
+  background-color: #ea4335;
+  position: absolute;
+  left: -1px;
+  right: 0;
+  pointer-events: none;
+
+  &.first::before {
+    content: '';
+    position: absolute;
+    background-color: #ea4335;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    margin-top: -5px;
+    margin-left: -6.5px;
+  }
+}
 ::v-deep .v-calendar-daily_head-day-label {
   display: none !important;
 }
