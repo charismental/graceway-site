d052da61b20afc8ab0576986331098c8da248b93
lordchuffnel <fs-j@live.com>
2021-02-12 18:44:03 -0800

    blog filter
    began scaffold of blog filter

---
+++ src/components/BlogFilter.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="blogfilter">
+    <v-container rounded="xl">
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card>
+            <v-card-title>
+              Search for blogs based on Category
+            </v-card-title>
+          </v-card>
+              <v-chip-group>
+                <v-chip
+                  v-for="b in blogs"
+                  :key="b.slug"
+                  label
+                >
+                  {{c.category}}
+                </v-chip>
+              </v-chip-group>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+  name: 'BlogFilter',
+  data: () => ({
+    // blogs: [],
+    // categories: [],
+  }),
+  props: {
+    blogs: {
+      type: Array,
+    },
+  },
+  components: {},
+  // methods: {
+  //   getAllCategories() {
+  //     const categoryUrl = 'https://gwrapi.herokuapp.com/category';
+  //     axios
+  //       .get(categoryUrl)
+  //       .then((res) => {
+  //         if (res?.data) {
+  //           this.categories = res.data;
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         // eslint-disable-next-line no-console
+  //         console.error(err);
+  //       });
+  //   },
+  //   getCategoryBlogs(category) {
+  //     if (typeof category === 'object' && category.blogs) {
+  //       category.blogs.forEach((b) => {
+  //         this.blogs.push(b);
+  //       });
+  //     }
+  //   },
+  // },
+  // created() {
+  //   this.getCategoryBlogs();
+  // },
+};
+</script>
+
+<style lang="scss" scoped>
+.blogfilter {
+  background: blue;
+  max-height: 20em;
+  min-height: 10em;
+}
+</style>
--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- <blog-filter></blog-filter> -->
     <router-view v-if="$route.name === 'Blog'"></router-view>
     <v-container v-else>
       <!-- <div v-for="(blog, i) in blogs" :key="i">{{ blog.author }}</div> -->
@@ -9,10 +10,11 @@
 
 <script>
 import axios from 'axios';
+// import BlogFilter from '../components/BlogFilter.vue';
 
 export default {
   name: 'Blogs',
-  components: {},
+  // components: { BlogFilter },
   data: () => ({
     reveal: false,
     blogs: [],
