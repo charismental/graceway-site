cb17acd42a08af7cc05cd993f6bc2662758470d8
frank <paintersmechanic@gmail.com>
2021-02-19 12:08:00 -0800

    moved features to api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <v-container
-    fluid
-    class="px-6"
-  >
+  <v-container fluid class="px-6">
     <v-carousel
       v-model="model"
       width="1200"
@@ -20,31 +17,24 @@
         :key="slide.src"
         eager
       >
-        <v-img
-          :src="`https://gracewayradio.com/slide/${slide.src}`"
-          height="100%"
-          eager
-        />
+        <v-img :src="`https://gracewayradio.com/slide/${slide.src}`" height="100%" eager />
       </v-carousel-item>
     </v-carousel>
     <v-row>
-      <v-col
-        cols="12"
-        md="4"
-      >
-        <v-card @click="
+      <v-col cols="12" md="4">
+        <v-card
+          @click="
             $router.push({
-              name: 'Blogs',
+              name: 'Blogs'
             })
-          ">
+          "
+        >
           <v-hover v-slot:default="{ hover }">
             <v-img src="~@/assets/blog.jpg">
               <v-expand-transition>
                 <div
                   class="d-flex transition-fast-in-fast-out cyan lighten-2 v-card--reveal"
-                  :style="
-                    hover || $vuetify.breakpoint.smAndDown ? 'height: 38%' : 'height: 5%'
-                  "
+                  :style="hover || $vuetify.breakpoint.smAndDown ? 'height: 38%' : 'height: 5%'"
                 >
                   <div
                     v-if="hover || $vuetify.breakpoint.smAndDown"
@@ -60,31 +50,16 @@
           </v-hover>
         </v-card>
       </v-col>
-      <v-col
-        cols="12"
-        md="4"
-      >
-        <v-dialog
-          :value="showVerse"
-          max-width="600"
-        >
+      <v-col cols="12" md="4">
+        <v-dialog :value="showVerse" max-width="600">
           <template v-slot:activator="{ on, attrs }">
-            <v-card
-              style="cursor: pointer"
-              v-bind="attrs"
-              v-on="on"
-              @click="activateVerse"
-            >
+            <v-card style="cursor: pointer" v-bind="attrs" v-on="on" @click="activateVerse">
               <v-hover v-slot:default="{ hover }">
                 <v-img src="~@/assets/verse.jpg">
                   <v-expand-transition>
                     <div
                       class="d-flex transition-fast-in-fast-out cyan lighten-2 v-card--reveal"
-                      :style="
-                        hover || $vuetify.breakpoint.smAndDown
-                          ? 'height: 38%'
-                          : 'height: 5%'
-                      "
+                      :style="hover || $vuetify.breakpoint.smAndDown ? 'height: 38%' : 'height: 5%'"
                     >
                       <div
                         v-if="hover || $vuetify.breakpoint.smAndDown"
@@ -106,19 +81,14 @@
           <router-view v-click-outside="closeVerse"></router-view>
         </v-dialog>
       </v-col>
-      <v-col
-        cols="12"
-        md="4"
-      >
+      <v-col cols="12" md="4">
         <v-card @click.stop="dialog = true">
           <v-hover v-slot:default="{ hover }">
             <v-img src="~@/assets/news.jpg">
               <v-expand-transition>
                 <div
                   class="d-flex transition-fast-in-fast-out cyan lighten-2 v-card--reveal"
-                  :style="
-                    hover || $vuetify.breakpoint.smAndDown ? 'height: 38%' : 'height: 5%'
-                  "
+                  :style="hover || $vuetify.breakpoint.smAndDown ? 'height: 38%' : 'height: 5%'"
                 >
                   <div
                     v-if="hover || $vuetify.breakpoint.smAndDown"
@@ -134,7 +104,7 @@
           </v-hover>
         </v-card>
         <v-dialog
-        dark
+          dark
           scrollable
           transition="dialog-bottom-transition"
           v-model="dialog"
@@ -142,49 +112,18 @@
           overlay-opacity=".8"
           max-width="350"
         >
-          <v-card v-model="dialogText"
-          color="#263238">
+          <v-card v-model="dialogText" color="#263238">
             <v-card-text class="mt-4 mb-4">
-            <v-img
-              contain
-              :src="require('@/assets/' + 'shaken.jpg')"
-            ></v-img>
-              <p>There is a lot of shaking going on in the body of Christ.
-                It is a deep shaking that may feel like our roots are being pulling
-                out. The shaking of God comes in the form of crisis, or hurt, or
-                some other form of physical or emotional pain. If that is happening to
-                you, ask yourself this question: Who is doing the shaking? Is it God
-                or is it Satan attacking?</p>
-              <p>Remember, as a Christian, even
-                if it is the Devil, God controls the work of Satan as it
-                affects the lives of His followers. If you are a child
-                of God and have committed yourself to the Lord then
-                you are under His control and His protection. There is a
-                call of God on the life of each Christian, to move from a place
-                where you worship golden idols, as the ancient Israelis did
-                at the base of Mount Sinai, to a place where you can have an
-                experience with God, alone, as Moses did at the top of that
-                same mountain. But that call demands a change in your life.</p>
-              <p>When God moves in your life change will occur. The question
-                is: will you cooperate with God or will He have to shake
-                you up a bit? Change calls for us to move out of the way of
-                pride and into the way of humility. And if you are not
-                willing to humble yourself on your own, to change your ways
-                , God will change you, and it may not be comfortable or
-                painless. When God shakes you, things get broken.</p>
-              <p>In honor of the 29th anniversary of Jamie Buckingham’s passing
-                from this earth into God's heavenly kingdom,Risky Living Ministries
-                is offering this e-book FREE. The offer is good through Feb. 21, 2021.</p>
-
-              <p>To obtain the e-book go to:</p>
-              <p>
-                <a href="https://www.amazon.com/dp/B08SLCS531?fbclid=
-          IwAR0LrHTfi2YPsbtFay_ALesGas5SjtSpWBzGz2teqLzgKiR-KbNk_On6bqc">
-                  Amazon Books: Shaken by God by Jamie Buckingham</a>
-              </p>
+              <v-img contain :src="image" class="mb-2"></v-img>
+              <div v-html="content"></div>
+              <div v-for="l in link" :key="l.id">
+                <a :href="l.link" target="_blank">
+                  Amazon Books: Shaken by God by Jamie Buckingham</a
+                >
+              </div>
             </v-card-text>
-            <v-card-actions @click="dialog=false">
-              <v-btn @click="dialog=false" color="primary">
+            <v-card-actions @click="dialog = false">
+              <v-btn @click="dialog = false" color="primary">
                 Close
               </v-btn>
             </v-card-actions>
@@ -192,17 +131,15 @@
         </v-dialog>
       </v-col>
     </v-row>
-    <v-snackbar
-      v-model="showSnackbar"
-      multi-line
-      color="warning"
-    >
+    <v-snackbar v-model="showSnackbar" multi-line color="warning">
       <div class="text-body-1 text-center">{{ snackBarText }}</div>
     </v-snackbar>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Home',
   data: () => ({
@@ -211,6 +148,9 @@ export default {
     dialog: false,
     dialogText: '',
     snackBarText: '',
+    content: '',
+    image: '',
+    link: [],
     slideShow: [
       // {
       //   src: '01 valentine.jpg',
@@ -262,6 +202,20 @@ export default {
     },
   },
   methods: {
+    getFeatures() {
+      const url = 'https://gwrapi.herokuapp.com/featurette/2';
+      axios
+        .get(url)
+        .then((res) => {
+          this.content = res.data.content;
+          this.image = res.data.image;
+          this.link = res.data.link;
+        })
+        .catch((err) => {
+          // eslint-disable-next-line no-console
+          console.error(err);
+        });
+    },
     navigateTo(slideObject) {
       if (slideObject?.link) {
         this.$router.push({ name: slideObject.link });
@@ -277,6 +231,9 @@ export default {
     activateVerse() {
       this.$router.push({ name: 'Verse', params: { verseId: this.dayOfYear } });
     },
+  },
+  mounted() {
+    this.getFeatures();
   },
 };
 </script>
