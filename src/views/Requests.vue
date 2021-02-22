9d66b3278e4b92023d795d0bd6e9603a58e392b4
Tristin Hewett <thewett-c@sbgtv.com>
2021-02-22 09:53:41 -0800

    Update for requests page, open simple modal, load img.

--- src/views/Requests.vue
+++ src/views/Requests.vue
@@ -1,37 +1,64 @@
 <template>
   <div class="requests">
-    <v-card style="position: relative" class="mx-auto" max-width="600">
+    <v-dialog v-model="openSongInfo" width="500">
+      <v-card color="#1F7087" dark>
+        <div class="d-flex flex-no-wrap justify-space-between">
+          <div>
+            <v-card-title class="headline">{{
+              activeSong && activeSong.title ? activeSong.title : ""
+            }}</v-card-title>
+
+            <v-card-subtitle>{{
+              activeSong && activeSong.artist ? activeSong.artist : ""
+            }}</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn @click="closeSongInfo" dark icon small>
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-btn
+                class="ml-2 text-capitalize"
+                outlined
+                rounded
+                @click="makeRequest(activeSong.songid)"
+                >Request Song</v-btn
+              >
+            </v-card-actions>
+          </div>
+
+          <v-avatar class="ma-3" size="125" tile>
+            <v-img :src="itemImg(activeSong)"></v-img>
+          </v-avatar>
+        </div>
+      </v-card>
+    </v-dialog>
+    <v-card class="mx-auto" max-width="600" max-height="800">
+      <v-card-text class="px-16">
+        <v-text-field
+          placeholder="Enter title or artist name..."
+          v-model="searchTerm"
+        ></v-text-field>
+      </v-card-text>
+      <v-card-text style="position: relative">
       <v-overlay absolute :value="searchLoading">
         <v-progress-circular indeterminate></v-progress-circular>
       </v-overlay>
-      <v-card-text>
-        <v-text-field
-          placeholder="Search if you dare..."
-          v-model="searchTerm"
-        ></v-text-field>
-        <v-list v-if="searchResults.length">
-          <v-list-item
-            style="cursor: pointer"
-            @click="makeRequest(item.songid)"
-            v-for="item in searchResults"
-            :key="item.songid"
-          >
-            {{ item.title }} - {{ item.artist }}
+        <v-list v-if="searchResults.length" height="600" style="overflow-y: auto">
+          <v-list-item v-for="item in searchResults" :key="item.songid">
+            <v-list-item-content>
+              <v-list-item-title style="cursor: pointer" @click="viewSongInfo(item)">
+                {{ item.title }}
+              </v-list-item-title>
+              <v-list-item-subtitle
+                style="cursor: pointer"
+                @click="searchTerm = item.artist"
+              >
+                {{ item.artist }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
           </v-list-item>
         </v-list>
       </v-card-text>
-    </v-card>
-    <v-card class="mx-auto mt-4" max-width="600">
-      <v-card-text>
-        <div class="px-6">
-          <v-text-field type="number" v-model.number="songId" />
-        </div>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn :loading="requestLoading" block color="primary" @click="makeRequest"
-          >Submit Request</v-btn
-        >
-      </v-card-actions>
     </v-card>
     <v-snackbar
       :timeout="-1"
@@ -48,12 +75,12 @@
 </template>
 
 <script>
-// requestHeader === 'Request Successful'
 import axios from 'axios';
 
 export default {
   name: 'Requests',
   data: () => ({
+    openSongInfo: false,
     searchTerm: '',
     searchResults: [],
     snackbar: false,
@@ -62,11 +89,12 @@ export default {
     requestBody: '',
     requestLoading: false,
     searchLoading: false,
+    activeSong: null,
   }),
   components: {},
   watch: {
     searchTerm(val) {
-      if (val.length >= 4) {
+      if (val.length >= 3) {
         this.fetchSongs();
       }
     },
@@ -80,6 +108,24 @@ export default {
     },
   },
   methods: {
+    itemImg(item) {
+      const url = 'https://gracewayradio.com/artwork/';
+      if (item?.picture) {
+        return url + item.picture;
+      }
+      if (this.loadingSongInfo) {
+        return `${url}loading.gif`;
+      }
+      return `${url}customMissing.jpg`;
+    },
+    viewSongInfo(songObj) {
+      this.activeSong = songObj;
+      this.openSongInfo = true;
+    },
+    closeSongInfo() {
+      this.openSongInfo = false;
+      this.activeSong = null;
+    },
     closeSnackbar() {
       this.snackbar = false;
       this.$nextTick(() => {
